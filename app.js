1c68b8b6f09fc517df750842a8903432b910e574
un-earthly <vijayalamin@gmail.com>
2022-06-22 10:48:36 -0700

    reviews routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const postsRouter = require('./routes/posts');
+const reviewsRouter = require('./routes/reviews');
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
+app.use('/reviews', reviewsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/reviews.js
+++ routes/reviews.js
@@ -2,24 +2,24 @@ const router = require('express').Router();
 
 /* GET all posts. */
 router.get('/', (req, res) => {
-  res.send('This is the  post');
+    res.send('This is the review');
 });
 /* GET specific posts. */
 router.get('/:id', (req, res) => {
-  console.log(req.params.id)
-  res.send('This is the review' + " " + req.params.id);
+    console.log(req.params.id)
+    res.send('This is the review' + " " + req.params.id);
 });
 /* POST listing posts. */
 router.post('/', (req, res) => {
-  res.send('This is the  post');
+    res.send('This is the review');
 });
 /* PATCH updating post. */
 router.patch('/:id', (req, res) => {
-  res.send('This is the  post');
+    res.send('This is the review');
 });
 /* DELETE posts listing. */
 router.delete('/:id', (req, res) => {
-  res.send('This is the  post');
+    res.send('This is the review');
 });
 
 
